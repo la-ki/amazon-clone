6fcde43314f175870733b90b71e5c6f448a6b548
Lazar <layarkostic94@gmail.com>
2020-09-09 21:18:02 +0200

    Added CheckoutProduct component

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './Checkout.css';
 
 import Subtotal from '../Subtotal/Subtotal';
+import { useStateValue } from '../../config/StateProvider';
+
+import CheckoutProduct from '../CheckoutProduct/CheckoutProduct';
 
 function Checkout() {
+
+    const [{basket}, dispatch] = useStateValue();
+
     return (
         <div className="checkout">
             <div className="checkout__left">
@@ -16,6 +22,15 @@ function Checkout() {
                     <h2 className="checkout__title">
                         Your shopping Basket
                     </h2>
+                    {basket.map(item => (
+                        <CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                        />
+                    ))}
                 </div>
             </div>
             <div className="checkout__right">
